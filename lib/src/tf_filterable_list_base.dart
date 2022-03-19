a10dbfecb9c22e098d4bc73b1b0c89074f34c54a
Rahul Badgujar <rahulbadgujar1508@gmail.com>
2022-03-19 21:30:05 +0530

    added doc to implementation file

--- lib/src/tf_filterable_list_base.dart
+++ lib/src/tf_filterable_list_base.dart
@@ -1,47 +1,92 @@
 import 'package:tf_data_streamer/tf_data_streamer.dart';
 
+/// Used to provide a items filter for list.
 typedef TfListItemFilter<T> = bool Function(T item);
 
+///
+/// TfFilterableList
+///
+/// A list having capabilities to filter the items for given filter. \
+/// Supports listening to the filtered list.
+///
 class TfFilterableList<T> extends TfDataStreamer<List<T>> {
+  // List of all items.
   final List<T> _allItems = [];
+  // List of filtered items.
   List<T> _filteredItems = [];
 
-  TfListItemFilter<T>? _currentFilter;
+  // Currently active filter to filter out items.
+  TfListItemFilter<T>? _activeFilter;
 
+  /// Get list of all the items considered for filtering.
   List<T> get allItems {
     return _allItems;
   }
 
+  /// Get list of all the items which pass the current filter.
+  ///
+  /// NOTE: If no current filter exists, this list is equivalent to allItems list.
   List<T> get filteredItems {
     return _filteredItems;
   }
 
+  /// Returns current active filter if exists, otherwise return null.
+  TfListItemFilter<T>? get activeFilter {
+    return _activeFilter;
+  }
+
+  /// Add more items to [allItems] list.
+  ///
+  /// This method should be followed by a call to apply()
+  /// to apply current filter on newly added items and notify the listeners.
   void addItems(List<T> newItemsList) {
     _allItems.addAll(newItemsList);
   }
 
+  /// Clears the list of all items. Also resulting in clearing of the filtered list as well.
+  ///
+  /// This method should be followed by a call to apply()
+  /// to notify the listeners about clearing of items.
   void clearAllItems() {
     _allItems.clear();
   }
 
+  /// Set a new filter for filtering all the items.
+  ///
+  /// If you wish that list should be filtered with this new filter,
+  /// this method call should be followed by apply() to apply the filter and notify listeners.
   void setFilter(TfListItemFilter filter) {
-    _currentFilter = filter;
+    _activeFilter = filter;
   }
 
+  /// Apply all the changes and refresh the filtered list.
+  /// Also, notify the listeners about changes in filtered list.
+  ///
+  /// Here, the changes can be any kind of change which may result in change of filtered list contents.
+  /// Like, change in active filter, addition of new items to all items list, clearing of all items list.
   void apply() {
     _filteredItems = _filterItemsFollowingFilter(
-        itemsList: _allItems, filter: _currentFilter);
+        itemsList: _allItems, filter: _activeFilter);
     _notifyFilteredItemsListChanges();
   }
 
+  /// Notifies listeners about changes in filtered list.
   void _notifyFilteredItemsListChanges() {
     addData(_filteredItems);
   }
 
+  // Clears the current filter.
+
+  /// If you wish that clearing of filter should reflect filtered list,
+  /// then this method call should be followed by apply() to update the filtered list and notify listeners.
   void clearFilter() {
-    _currentFilter = null;
+    _activeFilter = null;
   }
 
+  /// Utility method to apply given [filter] to [itemsList]
+  /// and return the list of items which follow given [filter].
+  ///
+  /// NOTE: if filter is null, the returned filtered list will match the [itemsList] provided.
   List<T> _filterItemsFollowingFilter(
       {required List<T> itemsList, TfListItemFilter<T>? filter}) {
     final filterOutputList = <T>[];
@@ -57,10 +102,12 @@ class TfFilterableList<T> extends TfDataStreamer<List<T>> {
     return filterOutputList;
   }
 
+  /// Stream streaming the filtered items list upon changes.
   Stream<List<T>> get streamOfFilteredItems {
     return stream;
   }
 
+  @Deprecated('Do nothing method.')
   @override
   void reload() {}
 }
